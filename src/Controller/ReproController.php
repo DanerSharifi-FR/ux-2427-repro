0883c09d9a6551cf22824d52e47177680e02f618
Daner Sharifi <daner.sharifi.official@gmail.com>
2026-09-16 16:40:09 +0200

    Reproduce Symfony UX autocomplete issue #2427 with remote data

--- src/Controller/ReproController.php
+++ src/Controller/ReproController.php
@@ -4,31 +4,60 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ReproController extends AbstractController
 {
+    private const FOODS = [
+        'cookies' => 'Cookies',
+        'cooking' => 'Cooking',
+        'coconut' => 'Coconut',
+        'tea' => 'Tea',
+        'apple' => 'Apple',
+    ];
+
     #[Route('/', name: 'app_repro')]
     public function index(): Response
     {
         $form = $this->createFormBuilder()
             ->add('foods', ChoiceType::class, [
                 'label' => 'Foods',
-                'choices' => [
-                    'Cookies' => 'cookies',
-                    'Cooking' => 'cooking',
-                    'Coconut' => 'coconut',
-                    'Tea' => 'tea',
-                    'Apple' => 'apple',
-                ],
+                'choices' => [],
                 'multiple' => true,
                 'autocomplete' => true,
+                'autocomplete_url' => $this->generateUrl('app_foods_autocomplete'),
             ])
             ->getForm();
 
         return $this->render('repro/index.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/foods-autocomplete', name: 'app_foods_autocomplete')]
+    public function autocomplete(Request $request): JsonResponse
+    {
+        $query = strtolower($request->query->getString('query'));
+
+        $results = [];
+
+        foreach (self::FOODS as $value => $label) {
+            if ('' !== $query && !str_contains(strtolower($label), $query)) {
+                continue;
+            }
+
+            $results[] = [
+                'value' => $value,
+                'text' => $label,
+            ];
+        }
+
+        return $this->json([
+            'results' => $results,
+            'next_page' => null,
         ]);
     }
 }
